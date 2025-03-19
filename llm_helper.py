7cca7985c89955f45a2680ff0b363bf3f7268e05
JAYASURYA <112236021+jayasurya2701@users.noreply.github.com>
2025-03-19 23:37:25 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -7,5 +7,5 @@ llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b
 
 
 if __name__ == "__main__":
-    response = llm.invoke("Capital of India ")
+    response = llm.invoke("What is the capital of India? ")
     print(response.content)
